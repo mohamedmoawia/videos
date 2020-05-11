c08652b32d3cf47af4d561131a9ef195cda95c78
mohamedmoawia <mo3wia94@gmail.com>
2020-05-11 22:12:23 +0200

    Updates

---
+++ precache-manifest.de072bd3fe8f61ecc9c3559cbdcea988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8f022e5aced072ae295f09bdbbf97d",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "ce6e31481640147e58ba",
+    "url": "/videos/static/css/main.628e3efa.chunk.css"
+  },
+  {
+    "revision": "999350962897af99a107",
+    "url": "/videos/static/js/2.c074e178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videos/static/js/2.c074e178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6e31481640147e58ba",
+    "url": "/videos/static/js/main.ffe0a03c.chunk.js"
+  },
+  {
+    "revision": "373cf2b69139c84bfd28",
+    "url": "/videos/static/js/runtime-main.ba7d5d3e.js"
+  }
+]);
